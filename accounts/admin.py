f00723b98eb78dbce46496ce82c42cc0970debb9
taha-asghari <taha1asghari@gmail.com>
2025-02-15 11:10:01 +0100

    some changes for book store project

---
+++ accounts/admin.py
@@ -0,0 +1,26 @@
+from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+
+from accounts.forms import CustomUserCreationForm, CustomUserChangeForm
+from accounts.models import CustomUser
+
+class CustomUserAdmin(UserAdmin):
+    model = CustomUser
+    add_form = CustomUserCreationForm
+    form = CustomUserChangeForm
+    fieldsets = UserAdmin.fieldsets + (
+        (None, {'fields': ('age',)}),
+    )
+    add_fieldsets = UserAdmin.add_fieldsets + (
+        (None, {'fields': ('age',)}),
+    )
+admin.site.register(CustomUser, CustomUserAdmin)
+
+
+
+
+
+
+
+
+
