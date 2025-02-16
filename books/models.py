f8bd8e6dae4739a64e54c7684f425823a46561e1
taha-asghari <taha1asghari@gmail.com>
2025-02-16 18:46:24 +0100

    Add some validation to Book model

--- books/models.py
+++ books/models.py
@@ -1,14 +1,26 @@
 from django.db import models
+from django.core.validators import MaxValueValidator, MinValueValidator, RegexValidator
+from django.utils.timezone import now
 
 
 class Book(models.Model):
-    title = models.CharField(max_length=100)
-    author = models.CharField(max_length=100)
-    description = models.TextField()
+    title = models.CharField(max_length=100, unique=True,  # Ensures no duplicate titles
+                             error_messages={"unique": "A book with this title already exists."})
+    author = models.CharField(max_length=100,
+                              validators=[
+                                  RegexValidator(r'^[a-zA-Z\s]+$', "Author name can only contain letters and spaces.")
+                              ])
+    description = models.TextField(blank=True, null=True)
     cover_image = models.ImageField(upload_to='covers/', default='default_image.jpg')
-    translator = models.CharField(max_length=100)
+    translator = models.CharField(max_length=100,blank=True,null=True, validators=[
+        RegexValidator(r'^[a-zA-Z\s]+$', "Author name can only contain letters and spaces.")
+    ])
     publisher = models.CharField(max_length=100)
-    publish_date = models.DateField()
+    publish_date = models.DateField(
+        validators=[MaxValueValidator(limit_value=now, message="Publish date cannot be in the future.")])
     genre = models.CharField(max_length=100)
-    pages_number = models.PositiveIntegerField()
-    price = models.DecimalField(max_digits=5, decimal_places=2)
+    pages_number = models.PositiveIntegerField(validators=[MinValueValidator(1, "Page number must be at least 1.")])
+    price = models.DecimalField(max_digits=5, decimal_places=2,validators=[MinValueValidator(0, "Price must be a positive number.")])
+
+    def __str__(self):
+        return self.title
