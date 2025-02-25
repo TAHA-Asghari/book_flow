7351c211a430835674e77c0ec9a9a101a5c7f7e6
taha-asghari <taha1asghari@gmail.com>
2025-02-25 17:21:03 +0100

    make relation with "comment" table to book table and register it on admin panel

--- books/admin.py
+++ books/admin.py
@@ -1,6 +1,12 @@
 from django.contrib import admin
-from .models import Book
+from .models import Book, Comment
+
 
 @admin.register(Book)
 class PostAdmin(admin.ModelAdmin):
     list_display = ('title', 'author', 'description', 'price')
+
+
+@admin.register(Comment)
+class PostAdmin(admin.ModelAdmin):
+    list_display = ('user', 'text', 'book', 'datetime_created')
--- books/forms.py
+++ books/forms.py
@@ -1,7 +1,13 @@
 from django import forms
-from .models import Book
+from .models import Book, Comment
+
 
 class NewBookForm(forms.ModelForm):
     class Meta:
         model = Book
+        fields = '__all__'
+
+class NewCommentForm(forms.ModelForm):
+    class Meta:
+        model = Comment
         fields = '__all__'
